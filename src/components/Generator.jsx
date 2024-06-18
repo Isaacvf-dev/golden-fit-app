9b21ecbf2757d0900b7cf50d880d20fbd161a994
Isaac Francelino <isaacvf.dev@gmail.com>
2024-06-18 18:05:05 -0300

    Main display done, working on generators

---
+++ src/components/Generator.jsx
@@ -0,0 +1,139 @@
+import { useState } from "react";
+import { SCHEMES, WORKOUTS } from "../utils/golden";
+import SectionWrapper from "./SectionWrapper";
+
+function Header({ index, title, description }) {
+  return (
+    <div className="flex flex-col gap-4">
+      <div className="flex items-center justify-center gap-2">
+        <p className="text-3xl sm:text-4xl md:text-5xl font-semibold text-slate-400">
+          {index}
+        </p>
+        <h4 className="text-xl sm:text-2xl md:text-3xl">{title}</h4>
+      </div>
+      <p className="text-sm sm:text-base text-center">{description}</p>
+    </div>
+  );
+}
+
+export default function Generator() {
+  const [showModal, setShowModal] = useState(false);
+  const [style, setStyle] = useState("individual");
+  const [muscles, setMuscles] = useState([]);
+  const [goal, setGoal] = useState("strength_power");
+
+  function toggleModal() {
+    setShowModal(!showModal);
+  }
+
+  function toggleStyle(type) {
+    setStyle(type);
+  }
+
+  function toggleGoal(scheme) {
+    setGoal(scheme);
+  }
+
+  function updateMuscles(muscleGroup) {
+    if (muscles.includes(muscleGroup)) {
+      setMuscles(muscles.filter(item => item !== muscleGroup))
+      return
+    }
+
+    if (muscles.length > 3) {
+      return
+    }
+
+    if (style !== 'individual') {
+      setMuscles([muscleGroup])      
+      setShowModal(!showModal)
+      return
+    }
+
+    setMuscles([...muscles, muscleGroup])
+    if (muscles.length === 3) {
+      setShowModal(false)
+    }
+  }
+  return (
+    <SectionWrapper
+      header={"create your workout"}
+      title={["It's", "Golden", "hour!"]}
+    >
+      <Header
+        index={"01"}
+        title={"Choose your style"}
+        description={"Select the muscles to be killed."}
+      />
+      <div className="grid grid-cols-2 sm:grid-cols-4 gap-4">
+        {Object.keys(WORKOUTS).map((type, typeIndex) => {
+          return (
+            <button
+              onClick={() => toggleStyle(type)}
+              className={`bg-slate-950 border  py-3 rounded-lg duration-200 hover:border-amber-600 ${
+                type === style ? "border-amber-600" : "border-amber-400"
+              }`}
+              key={typeIndex}
+            >
+              <p className="capitalize">{type.replace("_", " ")}</p>
+            </button>
+          );
+        })}
+      </div>
+
+      <Header
+        index={"02"}
+        title={"Picking targets"}
+        description={"Select the muscles for complete destruction."}
+      />
+      <div
+        className="bg-slate-950 border border-solid border-amber-400 rounded-lg flex flex-col"
+        
+      >
+        <button onClick={() => toggleModal()} className="relative p-3 flex items-center justify-center">
+          <p>Select muscle groups</p>
+          <i className="fa-solid fa-caret-down absolute right-3 top-1/2 -translate-y-1/2 "></i>
+        </button>
+        {showModal && (
+          <div className="px-3 pb-3 flex flex-col">
+            {(style === "individual"
+              ? WORKOUTS[style]
+              : Object.keys(WORKOUTS[style])
+            ).map((muscleGroup, muscleGroupIndex) => {
+              return (
+                <button
+                  onClick={() => updateMuscles(muscleGroup)}
+                  className={`hover:text-amber-400 duration-200 ${muscles.includes(muscleGroup) ? 'text-amber-400' : ''}`}
+                  key={muscleGroupIndex}
+                >
+                  <p className="uppercase">{muscleGroup.replace("_", " ")}</p>
+                </button>
+              );
+            })}
+          </div>
+        )}
+      </div>
+
+      <Header
+        index={"03"}
+        title={"Get your level up"}
+        description={"Select your main goal."}
+      />
+      <div className="grid grid-cols-3 gap-4">
+        {Object.keys(SCHEMES).map((scheme, schemeIndex) => {
+          return (
+            <button
+              onClick={() => toggleGoal(scheme)}
+              className={`bg-slate-950 border  py-3 rounded-lg duration-200 hover:border-amber-600 ${
+                scheme === goal ? "border-amber-600" : "border-amber-400"
+              }`}
+              key={schemeIndex}
+            >
+              <p className="capitalize">{scheme.replace("_", " ")}</p>
+            </button>
+          );
+        })}
+      </div>
+    </SectionWrapper>
+  );
+}
---
+++ src/components/Hero.jsx
@@ -0,0 +1,23 @@
+export default function Hero() {
+  return (
+    <div className="min-h-screen flex flex-col gap-10 items-center justify-center text-center max-w-[800px] mx-auto w-full p-4">
+      <div className="flex flex-col gap-4">
+        <p>NOW IT&apos;S YOUR TIME. START YOUR</p>
+        <h1 className="font-bold text-4xl sm:text-5xl md:text-6xl lg:text-7xl">
+          <span className="text-amber-400">GOLDEN</span> FITNESS JOURNEY!
+        </h1>
+      </div>
+
+      <p className="text-sm md:text-base font-light">
+        Ready to turn those &quot;maybe laters&quot; into &quot;today&apos;s
+        gains&quot;? It&apos;s time to <span className="text-amber-400 font-medium">level up</span> your fitness game
+        and crush those goals you didn&apos;t even know you had. <br/>Let the{" "}
+        <span className="text-amber-400 font-medium">gains</span> begin!
+      </p>
+
+      <button className="px-8 py-4 rounded-md border-[2px] bg-slate-950 border-amber-400 border-solid goldenShadow duration-200">
+        Accept & Begin
+      </button>
+    </div>
+  );
+}
